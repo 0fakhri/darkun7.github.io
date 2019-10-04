c04f196885dbfae464c117458b3449b4de27ef5d
darkun7 <182410101113@students.unej.ac.id>
2019-10-04 22:14:26 +0700

    Remove jquery Dependency.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,49 @@
 console.log("Main.JS Connected");
-  /*===Smooth Scroll===*/
-$(document).ready(function(){
-$("a").on('click', function(event) {
- if (this.hash !== "") {
-   event.preventDefault();
-   var hash = this.hash;
-   $('html, body').animate({
-     scrollTop: $(hash).offset().top
-   }, 800, function(){
-     window.location.hash = hash;
-   });
- }
-});
-});
+//   /*===Smooth Scroll===*/
+// $(document).ready(function(){
+// $("a").on('click', function(event) {
+//  if (this.hash !== "") {
+//    event.preventDefault();
+//    var hash = this.hash;
+//    $('html, body').animate({
+//      scrollTop: $(hash).offset().top
+//    }, 800, function(){
+//      window.location.hash = hash;
+//    });
+//  }
+// });
+// });
 
   /*===Navbar===*/
-$(document).ready(function(){
-var isActive = false;
-$('.menu').on('click', function(event) {
-  if (isActive){
-    $(this).removeClass('active');
-    $('body').removeClass('menu-open');
-  } else{
-    $(this).addClass('active');
-    $('body').addClass('menu-open');
+  // menu.addEventListener("click", function(){
+  var isActive = false;
+  function toggleNav(){
+    let menu = document.querySelector('.menu');
+    let body = document.querySelector('body');
+    // var menu = document.getElementsByClassName("menu");
+    // var body = document.getElementsByTagName("body");
+    // console.log("Signal "+ isActive);
+    if (isActive){
+      menu.classList.remove("active");
+      body.classList.remove("menu-open");
+    }else{
+      menu.classList.add("active");
+      body.classList.add("menu-open");
+    }
+    isActive = !isActive;
   }
-  isActive = !isActive;
-});
-});
+// );
+  // Ini Pas Pake Jquery
+// $(document).ready(function(){
+// var isActive = false;
+// $('.menu').on('click', function(event) {
+//   if (isActive){
+//     $(this).removeClass('active');
+//     $('body').removeClass('menu-open');
+//   } else{
+//     $(this).addClass('active');
+//     $('body').addClass('menu-open');
+//   }
+//   isActive = !isActive;
+// });
+// });
